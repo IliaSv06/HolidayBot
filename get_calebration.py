62c33a50e8162a471b90f8cc2394ce761d9fabeb
dumar <dumarzhev@gmail.com>
2023-05-24 15:30:03 +1000

    Функция возвращающая праздники сегодня get_celebration()

---
+++ get_calebration.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+URL = 'https://my-calend.ru/holidays'
+date = ''
+
+def get_html():
+    global date
+    with open('index.html', 'w', encoding='UTF-8') as file:
+        file.write(requests.get(URL).text)
+    date = datetime.date.today()
+
+def get_celebration():
+    global date
+    if datetime.date.today() != date:
+        get_html()
+    with open('index.html', 'r', encoding='UTF-8') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+
+    celebrations = [
+        item.text
+        for item in soup.select('ul.holidays-items span, ul.holidays-items a')
+        if item.text.strip() and not item.text.strip().isdigit()
+    ]
+
+    return celebrations
